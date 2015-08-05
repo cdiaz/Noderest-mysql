798cfc031055f425a008851eccbcf8cce5fa0195
cristiam <hola@quantux.co>
2015-08-05 17:53:11 -0500

    heroku mysql config HOST

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
 	    user 		: process.env.DB_USERNAME || 'root',
 	    password 	: process.env.DB_PASSWORD || 'root',
 	    database	: process.env.DB_NAME || 'apirest'
-	},
+	}
 }	
