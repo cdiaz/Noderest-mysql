386c8f368ef4510b1619af5d83594c15d39cfea9
cristiam <hola@quantux.co>
2015-08-05 18:06:11 -0500

    heroku mysql config DB

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 		
 	db	: {
-		    host: 'us-cdbr-iron-east-02.cleardb.net',
-		    user: 'b3e1fe953c2606',
-		    password: '664ce24e',
-		    database: 'heroku_bce5a5f738d4c6a'
+		    host: process.env.DB_HOST || 'us-cdbr-iron-east-02.cleardb.net',
+		    user: process.env.DB_USERNAME ||'b3e1fe953c2606',
+		    password: process.env.DB_PASSWORD || '664ce24e',
+		    database: process.env.DB_NAME || 'heroku_bce5a5f738d4c6a'
 	}
 }	
