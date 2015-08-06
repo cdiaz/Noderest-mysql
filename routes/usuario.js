e97ca78acae5b3133fddcf27c1187ac4f1e40d2e
cristiam <hola@quantux.co>
2015-08-05 22:54:53 -0500

    crud

--- routes/usuario.js
+++ routes/usuario.js
@@ -29,8 +29,64 @@ var userId= req.params.id || '';
     });
 };
 
-exports.saveUser = function(req,res,next){};
+exports.saveUser = function(req,res,next){
 
-exports.updateUser = function(req,res,next){};
+    var input = JSON.parse(JSON.stringify(req.body));
+    var Usuario = {     
+            id:'',
+            nombre: input.nombre,
+            ciudad: input.ciudad
+    };
 
-exports.deleteUser = function(req,res,next){};
+    req.getConnection(function(err,conn){
+       
+       var query = connection.query(
+        "INSERT INTO usuario set ?",Usuario,function(err,rows){
+              if(err){
+                console.log(err);
+                return next("Mysql Error");
+            }
+             res.json(rows);
+         });
+    });
+};
+
+exports.updateUser = function(req,res,next){
+
+    var userId= req.params.id || '';
+    var input = JSON.parse(JSON.stringify(req.body));
+
+      var Usuario = {     
+                nombre: input.nombre,
+                ciudad: input.ciudad
+        };
+
+    req.getConnection(function(err,conn){
+           
+           var query = connection.query(
+            "UPDATE usuario set ? where id = ?",[Usuario,userId],function(err,rows){
+                  if(err){
+                    console.log(err);
+                    return next("Mysql Error");
+                }
+                 res.json(rows);
+             });
+        });
+};
+
+
+exports.deleteUser = function(req,res,next){
+
+    var userId= req.params.id || '';
+
+    req.getConnection(function(err,conn){
+        if (err) return next("Error de conexión");
+        var query = conn.query('DELETE * FROM usuario WHERE id = ?',userId,function(err,rows){
+            if(err){
+                console.log(err);
+                return next("Mysql Error");
+            }
+             res.json(rows);
+         });
+    });
+};
